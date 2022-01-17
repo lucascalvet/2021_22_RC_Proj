ee77337e0034b46101a1bea96945a37866a67364
Lucas Calvet Santos <lucascalvet2000@hotmail.com>
2022-01-17 03:26:12 +0000

    Finished commands and replies

--- proj_2/download.c
+++ proj_2/download.c
@@ -8,6 +8,10 @@
 #include <unistd.h>
 #include <string.h>
 #include <regex.h>
+#include <error.h>
+#include <errno.h>
+#include <libgen.h>
+#include <fcntl.h>
 
 #define BUFFER_SIZE 256
 
@@ -33,14 +37,12 @@ int connection(char *server_address, int server_port)
     /*open a TCP socket*/
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("socket()");
         return -1;
     }
 
     /*connect to the server*/
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
-        perror("connect()");
         return -1;
     }
 
@@ -50,25 +52,39 @@ int connection(char *server_address, int server_port)
 /*
  * Reads a reply from the FTP server
  *
- * stream: the socket's file stream
  * reply_buffer: a pointer to store the address of the buffer containing the reply
  * reply_buffer_size: a pointer to store the allocated size of the buffer
+ * stream: the socket's file stream
+ * expected_code: the reply code that is expected
+ * error_msg: the message to show in case the reply code is not the expected one
  *
  * returns: On success, the number of characters read,
  * including the delimiter character, but not including the terminating null byte.
  * On error, -1 is returned, and errno is set to indicate the error.
  */
-int read_reply(FILE *stream, char **reply_buffer, int *reply_buffer_size)
+int read_reply(char **reply_buffer, size_t *reply_buffer_size, FILE *stream, char *expected_code, char *error_msg)
 {
     regex_t reply_regex;
-    regcomp(&reply_regex, "^[:digit:]{3}[:blank:]", REG_EXTENDED);
+    regcomp(&reply_regex, "^[0-9]{3} ", REG_EXTENDED | REG_NOSUB);
 
     ssize_t nread;
-    do
+    nread = getline(reply_buffer, reply_buffer_size, stream);
+    while (nread >= 0 && regexec(&reply_regex, *reply_buffer, 0, NULL, 0) != 0)
     {
         free(*reply_buffer);
+        *reply_buffer = NULL;
         nread = getline(reply_buffer, reply_buffer_size, stream);
-    } while (nread >= 0 && regexec(&reply_regex, reply_buffer, 0, NULL, 0) != 0);
+    }
+
+    if (nread == -1)
+    {
+        error(1, errno, "error reading the server's reply");
+    }
+
+    if (strncmp(*reply_buffer, expected_code, 3) != 0)
+    {
+        error(1, 0, "%s. Response: %s", error_msg, *reply_buffer);
+    }
 
     return nread;
 }
@@ -81,19 +97,18 @@ int main(int argc, char **argv)
         exit(-1);
     }
 
-    regex_t url_regex;
+    regex_t regex;
 
-    regcomp(&url_regex, "^ftp://((([[:alnum:]+.-][[:alnum:]+.-]*)(:([[:alnum:]+.-]*)){0,1}){0,1}@){0,1}([[:alnum:]+.-]*)/([[:alnum:]+./-]*)$", REG_EXTENDED);
+    regcomp(&regex, "^ftp://((([[:alnum:]+._-][[:alnum:]+._-]*)(:([[:alnum:]+._-]*)){0,1}){0,1}@){0,1}([[:alnum:]+._-]*)/([[:alnum:]+./_-]*)$", REG_EXTENDED);
 
     regmatch_t url_regmatch[8];
 
-    if (regexec(&url_regex, argv[1], 8, url_regmatch, 0) != 0)
+    if (regexec(&regex, argv[1], 8, url_regmatch, 0) != 0)
     {
         fprintf(stderr, "NO MATCH\nUsage: %s ftp://[<user>:<password>@]<host>/<url-path>\n", argv[0]);
         exit(-1);
     }
 
-    int no_password = 1;
     char *user, *password, *host, *path;
     int len;
 
@@ -118,12 +133,15 @@ int main(int argc, char **argv)
     /* Check if the password is present */
     if (url_regmatch[4].rm_so != -1)
     {
-        no_password = 0;
-
         len = url_regmatch[5].rm_eo - url_regmatch[5].rm_so;
         password = (char *)malloc((len + 1) * sizeof(char));
         memcpy(password, argv[1] + url_regmatch[5].rm_so * sizeof(char), len);
         password[len] = 0;
+    }
+    else
+    {
+        password = (char *)malloc(1 * sizeof(char));
+        memcpy(password, "", 1);
     }
 
     len = url_regmatch[6].rm_eo - url_regmatch[6].rm_so;
@@ -136,124 +154,113 @@ int main(int argc, char **argv)
     memcpy(path, argv[1] + url_regmatch[7].rm_so * sizeof(char), len);
     path[len] = 0;
 
-    printf("User: %s\nPassword: %s\nHost: %s\nPath: %s\n", user, password, host, path);
+    printf("Parsed input:\n\tUser: %s\n\tPass: %s\n\tHost: %s\n\tPath: %s\n\n", user, password, host, path);
 
-    int port = 21;
     struct hostent *h;
 
     if ((h = gethostbyname(host)) == NULL)
     {
         error(1, errno, "error getting hostname");
     }
+    free(host);
 
-    printf("Host name  : %s\n", h->h_name);
-    printf("IP Address : %s\n", inet_ntoa(*((struct in_addr *)h->h_addr)));
+    printf("Resolved host:\n\tHost name: %s\n\tIP Address: %s\n", h->h_name, inet_ntoa(*((struct in_addr *)h->h_addr)));
 
-    int socket_fd = connection(inet_ntoa(*((struct in_addr *)h->h_addr)), port);
+    int socket_fd = connection(inet_ntoa(*((struct in_addr *)h->h_addr)), 21);
 
     if (socket_fd == -1)
     {
-        perror("connection()");
-        exit(-1);
+        error(1, errno, "connection()");
     }
 
     char *reply = NULL;
     size_t reply_len = 0;
     FILE *fp = fdopen(socket_fd, "r");
 
-    if (read_reply(fp, &reply, &reply_len) == -1) {
-        error(1, errno, "error reading the server's reply");
-    }
-
-    if (reply[0] == '2')
-    {
-        /*
-        Write username
-        */
-    }
-    else
-    {
-        error(1, 0, "server not ready for commands. Response: %s", reply);
-    }
-
-    /* Get 331, then write password */
-
-    /* Get 227 and parse the returned ip */
-
+    read_reply(&reply, &reply_len, fp, "220", "server not ready for commands");
     free(reply);
     reply = NULL;
 
-    regex_t ip_regex;
-    regcomp(&ip_regex, "\(([:digit:]*),([:digit:]*),([:digit:]*),([:digit:]*),([:digit:]*),([:digit:]*)\)", REG_EXTENDED);
+    dprintf(socket_fd, "user %s\r\n", user);
+    read_reply(&reply, &reply_len, fp, "331", "login was unsuccessful");
+    free(reply);
+    reply = NULL;
+    free(user);
+
+    dprintf(socket_fd, "pass %s\r\n", password);
+    read_reply(&reply, &reply_len, fp, "230", "login was unsuccessful");
+    free(reply);
+    reply = NULL;
+    free(password);
+
+    dprintf(socket_fd, "pasv\r\n");
+    read_reply(&reply, &reply_len, fp, "227", "error entering passive mode");
+
+    regcomp(&regex, "\\(([0-9]*),([0-9]*),([0-9]*),([0-9]*),([0-9]*),([0-9]*)\\)", REG_EXTENDED);
 
     regmatch_t ip_regmatch[7];
 
-    char *ip_string;
-
-    if (regexec(&ip_regex, ip_string, 7, ip_regmatch, 0) != 0)
+    if (regexec(&regex, reply, 7, ip_regmatch, 0) != 0)
     {
-        fprintf(stderr, "NO MATCH\n");
-        exit(-1);
+        error(1, 0, "there was no match for an ip and port in 227 response. Response: %s", reply);
     }
 
-    len = url_regmatch[3].rm_eo - url_regmatch[3].rm_so;
-    user = (char *)malloc((len + 1) * sizeof(char));
-    memcpy(user, argv[1] + url_regmatch[3].rm_so * sizeof(char), len);
+    len = ip_regmatch[4].rm_eo - ip_regmatch[1].rm_so;
+    char *ip, *port_1, *port_2;
 
-    int temp_len, total_len = 4;
-    char *temp_ip, *ip, *port_1, *port_2;
+    ip = (char *)malloc((len + 1) * sizeof(char));
 
-    for (int i = 0; i < 4; i++)
-    {
-        total_len += url_regmatch[i + 1].rm_eo - url_regmatch[i + 1].rm_so;
-    }
+    memcpy(ip, reply + ip_regmatch[1].rm_so * sizeof(char), len);
+    ip[ip_regmatch[1].rm_eo - ip_regmatch[1].rm_so] = '.';
+    ip[ip_regmatch[2].rm_eo - ip_regmatch[1].rm_so] = '.';
+    ip[ip_regmatch[3].rm_eo - ip_regmatch[1].rm_so] = '.';
+    ip[len] = 0;
 
-    ip = (char *)malloc(total_len * sizeof(char));
+    len = ip_regmatch[5].rm_eo - ip_regmatch[5].rm_so;
+    port_1 = (char *)malloc((len + 1) * sizeof(char));
+    memcpy(port_1, reply + ip_regmatch[5].rm_so * sizeof(char), len);
+    port_1[len] = 0;
+    len = ip_regmatch[6].rm_eo - ip_regmatch[6].rm_so;
+    port_2 = (char *)malloc((len + 1) * sizeof(char));
+    memcpy(port_2, reply + ip_regmatch[6].rm_so * sizeof(char), len);
+    port_2[len] = 0;
 
-    for (int i = 0; i < 4; i++)
-    {
-        temp_len = url_regmatch[i + 1].rm_eo - url_regmatch[i + 1].rm_so;
-        temp_ip = (char *)malloc((temp_len + 1) * sizeof(char));
-        memcpy(temp_ip, ip_string + url_regmatch[i + 1].rm_so * sizeof(char), temp_len);
-        strcat(ip, temp_ip);
-        if (i < 3)
-        {
-            strcat(ip, ".");
-        }
-    }
-    free(temp_ip);
-
-    temp_len = url_regmatch[5].rm_eo - url_regmatch[5].rm_so;
-    port_1 = (char *)malloc((temp_len + 1) * sizeof(char));
-    memcpy(port_1, ip_string + url_regmatch[5].rm_so * sizeof(char), temp_len);
-    temp_len = url_regmatch[6].rm_eo - url_regmatch[6].rm_so;
-    port_2 = (char *)malloc((temp_len + 1) * sizeof(char));
-    memcpy(port_2, ip_string + url_regmatch[6].rm_so * sizeof(char), temp_len);
-
-    int given_port = 256 * atoi(port_1) + atoi(port_2);
+    int port = 256 * atoi(port_1) + atoi(port_2);
 
     free(port_1);
     free(port_2);
+    free(reply);
+    reply = NULL;
 
-    char readbuf[BUFFER_SIZE];
+    printf("\tFile address: %s:%d\n\n", ip, port);
+
     char *filename = basename(path);
 
-    int file_socket_fd = connection(ip, given_port);
+    int file_socket_fd = connection(ip, port);
     if (file_socket_fd == -1)
     {
         error(1, errno, "error connecting to file server");
     }
+    free(ip);
 
+    dprintf(socket_fd, "retr %s\r\n", path);
+    read_reply(&reply, &reply_len, fp, "150", "error retrieving requested file");
+    free(reply);
+    reply = NULL;
+
+    printf("Saving to %s...\n", filename);
     int output_fd = creat(filename, 666);
     if (output_fd == -1)
     {
         error(1, errno, "error creating new file");
     }
-
-    ssize_t nread;
+    free(path);
+    
+    char readbuf[BUFFER_SIZE];
+    size_t nread;
     while ((nread = read(file_socket_fd, readbuf, BUFFER_SIZE)) > 0)
     {
-        if (write(output_fd, readbuf, BUFFER_SIZE) == -1)
+        if (write(output_fd, readbuf, nread) == -1)
         {
             error(1, errno, "error writing to the new file");
         }
@@ -274,19 +281,17 @@ int main(int argc, char **argv)
         error(1, errno, "error closing the connection to the file server");
     }
 
+    read_reply(&reply, &reply_len, fp, "226", "error completing transfer");
+    free(reply);
+    reply = NULL;
+    dprintf(socket_fd, "quit\r\n");
+
     if (close(socket_fd) == -1)
     {
         error(1, errno, "error closing the connection to the server");
     }
 
-    free(user);
-    free(password);
-    free(host);
-    free(path);
-    free(ip);
-
-    printf("Success!\n");
+    printf("Transfer completed! Exiting.\n");
     return 0;
 }
 
-//fscanf(%d%s, &numb, null)
